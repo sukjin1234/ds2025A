065ca3af3d9dca38c0ebeb4151a8f45eff15a2ae
sukjin1234 <tjrwls5661@naver.com>
2025-03-17 19:36:10 +0900

    v0.3 O(log n) Guess game

--- week02.py
+++ week02.py
@@ -1,3 +1,19 @@
-n = int(input())
-result = n * (n+1) // 2 # 상수 시간을 갖는다 O(1)
-print(result)
+import random
+
+n = random.randrange(1,101)
+win = True
+for i in range(7,0,-1):
+    guess = int(input(f"{i}번 남았습니다. 정답을 입력하세요 : "))
+    if guess == n:
+        win = True
+        break
+    else:
+        if guess > n:
+            print("정답보다 큽니다.")
+        else:
+            print("정답보다 작습니다.")
+        win = False
+if win:
+    print("정답입니다.")
+else:
+    print(f"정답은 {n}이였습니다.")
