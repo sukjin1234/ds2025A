4e3005de79a9d4972eae67c9916e7d0f77ac4a70
sukjin1234 <tjrwls5661@naver.com>
2025-03-24 17:06:26 +0900

    v0.5 array

--- week03.py
+++ week03.py
@@ -1,3 +1,5 @@
-l = [99,0,-7,0,16]
-for i in range(len(l)):
-    print(f"{l[i]:3},{id(l[i])}")
+import array
+# 같은 주소에 저장됨
+arr = array.array("f",[99,0,-7,0,16])
+for i in range(len(arr)):
+    print(f"{arr[i]:3},{id(arr[i])}")
