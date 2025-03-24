cd4d0e2212b6a66b32984972fc5e6a8e4c839d61
sukjin1234 <tjrwls5661@naver.com>
2025-03-24 17:58:09 +0900

    v0.8 array

--- week03.py
+++ week03.py
@@ -1,4 +1,25 @@
-groups = ["HOT","Seventeen","Black Pink","NJZ"]
-ratings = [1,2,4,3]
-group_rating = zip(groups,ratings)
-print(list(group_rating))
+# set은 순서가 없음
+
+def duplicate_city(cites):
+    result_city = []
+    s = set()
+
+    for city in cites:
+        l1 = len(s)
+        s.add(city)
+        l2 = len(s)
+        if l1 == l2:
+            result_city.append(city)
+
+    return set(result_city)
+
+
+cities = ["Incheon","Seoul","Incheon","Incheon","Gwangju"]
+# cities = set(cities)
+# cities.add("Incheon")
+# cities.add("Suwon")
+cities.append("Suwon")
+cities.append("Incheon")
+cities.append("Seoul")
+print(cities)
+print(duplicate_city(cities))
