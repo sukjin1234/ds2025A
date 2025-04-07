38c2d9d919c52eef3ff0f4af518e5611b59a8ac9
sukjin1234 <tjrwls5661@naver.com>
2025-04-07 18:36:35 +0900

    is_valid_parentheses update

--- week05.py
+++ week05.py
@@ -1,18 +1,22 @@
 # print(1+2))
+
+baskets = {"]":"[","}":"{",")":"("}
                         # 실제 값이 달라도 오류가 안남
 def is_valid_parentheses(expression : str) -> bool: #type hint
     stack = list()
     for letter in expression:
-        if letter == "(":
+        if letter in baskets.values():
             stack.append(letter)
-        if letter == ")":
+        if letter in baskets.keys():
             if len(stack) == 0:
+                return  False
+            result = stack.pop()
+            if baskets.get(letter) != result:
                 return False
-            else:
-                stack.pop()
+
     return len(stack) == 0
 
-print(is_valid_parentheses(")(1+2))"))
+print(is_valid_parentheses("[(1+2])"))
 print(is_valid_parentheses("(1+2))"))
 print(is_valid_parentheses("(1+2)"))
 print(is_valid_parentheses("((3*2)/2)"))
