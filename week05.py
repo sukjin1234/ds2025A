9b0407e3005ef0312134d11e3d491505ee361c90
sukjin1234 <tjrwls5661@naver.com>
2025-04-07 17:50:10 +0900

    stack as list

--- week05.py
+++ week05.py
@@ -1,37 +1,9 @@
-class Node:
-    def __init__(self,data,link=None):
-        self.data = data
-        self.link = link
-
-class Stack:
-    def __init__(self):
-        self.top = None
-
-    def push(self,data):
-        node = Node(data)
-        if self.top is None:
-            self.top = node
-        else:
-            node.link = self.top
-            self.top = node
-
-    def pop(self):
-        if self.top is None:
-            return "Stack is empty!"
-            #raise IndexError("스택이 비어 있습니다.")
-        popped_node = self.top
-        self.top = self.top.link
-        popped_node.link = None
-        return  popped_node.data
-
-    def peek(self):
-        return self.top.data
-
-s1 = Stack()
+s1 = []
+s1.append("Data Structure") # push
+s1.append("DataBase")
+print(s1[-1]) # peek
+print(s1)
 print(s1.pop())
-s1.push("Data Struct")
-s1.push("DataBase")
-print(s1.peek())
+print(s1)
 print(s1.pop())
-print(s1.pop())
-print(s1.pop())
+print(s1)
