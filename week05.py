62c09ef646c2c0768930ad627e98bf5f5ccd191c
sukjin1234 <tjrwls5661@naver.com>
2025-04-07 18:17:59 +0900

    is_valid_parentheses stack

--- week05.py
+++ week05.py
@@ -1,9 +1,18 @@
-s1 = []
-s1.append("Data Structure") # push
-s1.append("DataBase")
-print(s1[-1]) # peek
-print(s1)
-print(s1.pop())
-print(s1)
-print(s1.pop())
-print(s1)
+# print(1+2))
+                        # 실제 값이 달라도 오류가 안남
+def is_valid_parentheses(expression : str) -> bool: #type hint
+    stack = list()
+    for letter in expression:
+        if letter == "(":
+            stack.append(letter)
+        if letter == ")":
+            if len(stack) == 0:
+                return False
+            else:
+                stack.pop()
+    return len(stack) == 0
+
+print(is_valid_parentheses(")(1+2))"))
+print(is_valid_parentheses("(1+2))"))
+print(is_valid_parentheses("(1+2)"))
+print(is_valid_parentheses("((3*2)/2)"))
