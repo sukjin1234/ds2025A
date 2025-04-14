92698b2b13ad5b3bae1ca6fbbb77c483faa6559d
sukjin1234 <tjrwls5661@naver.com>
2025-04-14 17:19:46 +0900

    queue

--- week06.py
+++ week06.py
@@ -19,7 +19,20 @@ class Queue:
             self.rear.link = node
             self.rear = node
 
+    def dequeue(self):
+        if self.front is None:
+            raise IndexError("Queue is empty")
+        self.size -= 1
+        temp = self.front
+        self.front = self.front.link
+        if self.front is None:
+            self.rear = None
+        return temp.data
+
 q = Queue()
 q.enqueue("Data structure")
 q.enqueue("Data base")
 print(q.size, q.front.data, q.rear.data)
+print(q.dequeue())
+print(q.dequeue())
+print(q.dequeue())
