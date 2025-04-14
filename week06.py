04a9d60d91f1fd52e8a497471e1ac9050a117ef1
sukjin1234 <tjrwls5661@naver.com>
2025-04-14 18:01:15 +0900

    package queue

--- week06.py
+++ week06.py
@@ -1,39 +1,12 @@
-class Node:
-    def __init__(self,data,link=None):
-        self.data = data
-        self.link = link
-
-class Queue:
-    def __init__(self):
-        self.front = None
-        self.rear = None
-        self.size = 0
-
-    def enqueue(self,data):
-        self.size += 1
-        node = Node(data)
-        if self.rear is None:
-            self.front = node
-            self.rear = node
-        else:
-            self.rear.link = node
-            self.rear = node
-
-    def dequeue(self):
-        if self.front is None:
-            raise IndexError("Queue is empty")
-        self.size -= 1
-        temp = self.front
-        self.front = self.front.link
-        if self.front is None:
-            self.rear = None
-        temp.link = None
-        return temp.data
+from  queue import Queue
 
 q = Queue()
-q.enqueue("Data structure")
-q.enqueue("Data base")
-print(q.size, q.front.data, q.rear.data)
-print(q.dequeue())
-print(q.dequeue())
-print(q.size, q.front, q.rear)
+q.put("Data structure")
+q.put("Data base")
+q.put("Java script")
+print(q.qsize())
+print(q.get())
+print(q.qsize())
+print(q.get())
+print(q.qsize())
+print(q.get())
