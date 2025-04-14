07bc97002fec62aa0ac3c3ee4ed219acb5902970
sukjin1234 <tjrwls5661@naver.com>
2025-04-14 17:12:52 +0900

    queue

---
+++ week06.py
@@ -0,0 +1,25 @@
+class Node:
+    def __init__(self,data,link=None):
+        self.data = data
+        self.link = link
+
+class Queue:
+    def __init__(self):
+        self.front = None
+        self.rear = None
+        self.size = 0
+
+    def enqueue(self,data):
+        self.size += 1
+        node = Node(data)
+        if self.rear is None:
+            self.front = node
+            self.rear = node
+        else:
+            self.rear.link = node
+            self.rear = node
+
+q = Queue()
+q.enqueue("Data structure")
+q.enqueue("Data base")
+print(q.size, q.front.data, q.rear.data)
