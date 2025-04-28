2a0d7d195cb86c5ad5cdd7a7dd05147989de6471
sukjin1234 <tjrwls5661@naver.com>
2025-04-28 18:08:32 +0900

    binary tree insert method

--- week08.py
+++ week08.py
@@ -26,29 +26,49 @@ class TreeNode:
 		self.data = None
 		self.right = None
 
+def insert(root, value):
+    node = TreeNode()
+    node.data = value
+
+    if root is None:
+        return node
+
+    current = root
+    while True:
+        if value < current.data:
+            if current.left is None:
+                current.left = node
+                break
+            current = current.left # 이동
+        else:
+            if current.right is None:
+                current.right = node
+                break
+            current = current.right # 이동
+    return root
+
 if __name__ == "__main__":
-    numbers = [10,15,8,3,9]
+    numbers = [10,15,8,3,9,100,7,13]
     root = None
 
-    node = TreeNode()
-    node.data = numbers[0] # 10
-    root = node
+    for number in numbers:
+        root = insert(root,number)
 
-    for number in numbers[1:]:
-        node = TreeNode()
-        node.data = number
+    in_order(root)
 
-        current = root
-        while True:
-            if number < current.data:
-                if current.left is None:
-                    current.left = node
-                    break
-                current = current.left # 이동
-            else:
-                if current.right is None:
-                    current.right = node
-                    break
-                current = current.right # 이동
-
-    pre_order(root)
+    # find_number = int(input())
+    # current = root
+    # while True:
+    #     if find_number == current.data:
+    #         print(f"{find_number}을(를) 찾았습니다")
+    #         break
+    #     elif find_number < current.data:
+    #         if current.left is None:
+    #             print(f"{find_number}이(가) 존재하지 않습니다")
+    #             break
+    #         current = current.left
+    #     else:
+    #         if current.right is None:
+    #             print(f"{find_number}이(가) 존재하지 않습니다")
+    #             break
+    #         current = current.right
