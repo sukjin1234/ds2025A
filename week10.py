730e28434223fc8e0b970523dae0c678b723301a
sukjin1234 <tjrwls5661@naver.com>
2025-05-12 16:47:15 +0900

    tree

---
+++ week10.py
@@ -0,0 +1,78 @@
+def pre_order(node):
+    if node is None:
+        return
+    print(node.data, end="-> ")
+    pre_order(node.left)
+    pre_order(node.right)
+
+
+def post_order(node):
+    if node is None:
+        return
+    post_order(node.left)
+    post_order(node.right)
+    print(node.data, end="-> ")
+
+def in_order(node):
+    if node is None:
+        return
+    in_order(node.left)
+    print(node.data, end="-> ")
+    in_order(node.right)
+
+class TreeNode:
+	def __init__(self):
+		self.left = None
+		self.data = None
+		self.right = None
+
+def search():
+    print()
+    find_number = int(input("찾는 수는 ? : "))
+    current = root
+    while True:
+        if find_number == current.data:
+            print(f"{find_number}을(를) 찾았습니다")
+            break
+        elif find_number < current.data:
+            if current.left is None:
+                print(f"{find_number}이(가) 존재하지 않습니다")
+                break
+            current = current.left
+        else:
+            if current.right is None:
+                print(f"{find_number}이(가) 존재하지 않습니다")
+                break
+            current = current.right
+
+def insert(root, value):
+    node = TreeNode()
+    node.data = value
+
+    if root is None:
+        return node
+
+    current = root
+    while True:
+        if value < current.data:
+            if current.left is None:
+                current.left = node
+                break
+            current = current.left # 이동
+        else:
+            if current.right is None:
+                current.right = node
+                break
+            current = current.right # 이동
+    return root
+
+if __name__ == "__main__":
+    numbers = [10,15,8,3,9,100,7,13]
+    root = None
+
+    for number in numbers:
+        root = insert(root,number)
+
+    in_order(root)
+
+    search()
