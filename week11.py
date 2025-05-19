1bc0c2451b5c54c779d6d137871482608c473dee
sukjin1234 <tjrwls5661@naver.com>
2025-05-19 18:03:05 +0900

    graph dfs method

--- week11.py
+++ week11.py
@@ -1,40 +1,26 @@
-class Graph:
-    def __init__(self, size):
-        self.SIZE = size
-        self.graph = [[0 for _ in range(size)] for _ in range(size)]
+graph = [
+    [0, 1, 1, 0, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 0],
+    [0, 1, 1, 0, 1, 1, 1, 0],
+    [0, 0, 0, 1, 0, 1, 0, 0],
+    [0, 0, 0, 1, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 1, 0]
+]       # 무방향 그래프 : 2차원 리스트로 표현했을 때 대칭이다
+# DFS : 깊이 우선 탐색
+def dfs(g,i,visited):
+    visited[i] = True
+    print(chr(ord('A')+i),end=" ")
+    for j in range(len(graph)):
+        if g[i][j] == 1 and not visited[j]:
+            dfs(g,j,visited)
 
-    def printGraph(self):
-        for r in range(self.SIZE):
-            for c in range(self.SIZE):
-                print(self.graph[r][c], end=' ')
-            print()
+# BFS : 너비 우선 탐색
+def bfs(g, i, visited):
+    pass
 
-G1 = Graph(4)
-G3 = Graph(4)
-G_self = Graph(4)
-# 0 == A, 1 == B, 2 == C, 3 == D
-G1.graph[0][1] = 1; G1.graph[0][2] = 1; G1.graph[0][3] = 1
-G1.graph[1][0] = 1; G1.graph[1][2] = 1
-G1.graph[2][0] = 1; G1.graph[2][1] = 1; G1.graph[2][3] = 1
-G1.graph[3][0] = 1; G1.graph[3][2] = 1
-
-print("G1 무방향 그래프")
-G1.printGraph()
-
-
-# 0 == A, 1 == B, 2 == C, 3 == D
-G3.graph[0][1] = 1; G3.graph[0][2] = 1
-G3.graph[3][0] = 1; G3.graph[3][2] = 1
-
-print("G3 방향 그래프")
-G3.printGraph()
-
-
-# 0 == A, 1 == B, 2 == C, 3 == D
-G_self.graph[0][3] = 1
-G_self.graph[1][2] = 1; G_self.graph[1][3] = 1
-G_self.graph[2][1] = 1
-G_self.graph[3][0] = 1; G_self.graph[3][1] = 1
-
-print("G_self 무방향 그래프")
-G_self.printGraph()
+visited_dfs = [False for _ in range(len(graph))]
+visited_bfs = [False for _ in range(len(graph))]
+dfs(graph,3,visited_dfs)
+bfs(graph,4,visited_bfs)
