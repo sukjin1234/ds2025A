628240f28039d920b7cd437891110ef21feb4079
sukjin1234 <tjrwls5661@naver.com>
2025-05-26 17:04:55 +0900

    dfs deque

--- week12.py
+++ week12.py
@@ -1,8 +1,37 @@
 from collections import deque
 
-d = deque([91,17,33])
-d.append(-14)
-d.appendleft(100)
-d.append(99)
-for _ in range(len(d)):
-    print(d.popleft())
+graph = [
+    [0, 1, 1, 0, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 0],
+    [0, 1, 1, 0, 1, 1, 1, 0],
+    [0, 0, 0, 1, 0, 1, 0, 0],
+    [0, 0, 0, 1, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 1, 0]
+]       # 무방향 그래프 : 2차원 리스트로 표현했을 때 대칭이다
+# DFS : 깊이 우선 탐색
+def dfs(g,i,visited):
+    visited[i] = True
+    print(chr(ord('A')+i),end=" ")
+    for j in range(len(graph)):
+        if g[i][j] == 1 and not visited[j]:
+            dfs(g,j,visited)
+
+# BFS : 너비 우선 탐색
+def bfs(g, i, visited):
+    queue = deque([i])
+    visited[i] = 1
+    # while len(queue) != 0:
+    while queue:
+        i = queue.popleft()
+        print(chr(ord('A')+i), end='')
+        for j in range(len(graph)):
+            if g[i][j] == 1 and not visited[j]:
+                queue.append(j)
+                visited[j]=1
+
+visited_dfs = [False for _ in range(len(graph))]
+visited_bfs = [0 for _ in range(len(graph))]
+dfs(graph,3,visited_dfs)
+bfs(graph,4,visited_bfs)
